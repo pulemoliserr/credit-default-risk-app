3f6ce8a5e19371678ca849c767ef27e690bd0531
pulemoliserr <pulemoliserr@gmail.com>
2026-07-17 22:58:00 +0200

    Update app.py

--- app.py
+++ app.py
@@ -167,7 +167,7 @@ if submit_button:
     with col2:
         st.subheader("💰 Cost Matrix Framework")
         st.markdown("""
-        Under Professor Oziel's underwriting constraints, errors carry asymmetric financial penalties:
+        As we learnt from the SECOM project, errors carry asymmetric financial penalties:
         * **Type I Error (False Alarm):** $100  
         * **Type II Error (Missed Default):** $400  
         """)
